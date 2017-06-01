7d5c4a319e546a8ef39b66db4b7e64f94d48d6aa
guilhermegff <triclorotolueno@hotmail.com>
2017-05-31 18:25:25 -0700

    Third Heroku Commit

--- app_monit/urls.py
+++ app_monit/urls.py
@@ -19,10 +19,10 @@ from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    url(r'^twitter_monitor.herokuapp.com/admin/', include(admin.site.urls)),
-    url(r'^$', RedirectView.as_view(url='/twitter_monitor.herokuapp.com/monitoramentos/', permanent=True)),
-    url(r'^twitter_monitor.herokuapp.com/accounts/', include('django.contrib.auth.urls')),
-    url(r'^twitter_monitor.herokuapp.com/', include('twitter_monitor.urls', namespace='monitoramento')),
+    url(r'^twittermonitor.herokuapp.com/admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(url='/twittermonitor.herokuapp.com/monitoramentos/', permanent=True)),
+    url(r'^twittermonitor.herokuapp.com/accounts/', include('django.contrib.auth.urls')),
+    url(r'^twittermonitor.herokuapp.com/', include('twitter_monitor.urls', namespace='monitoramento')),
 ]
 
 from django.conf import settings
