085fe0505c2bc381bf4f042b8758f3b898ffa7c1
guilhermegff <triclorotolueno@hotmail.com>
2017-05-31 20:37:32 -0700

    Eighth Heroku Commit

--- app_monit/settings.py
+++ app_monit/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+#SECRET_KEY = 'syf(by#w(+c#yne&n1460r0rue)k*_v5z84ynh3ueg&n88)c83'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'syf(by#w(+c#yne&n1460r0rue)k*_v5z84ynh3ueg&n88)c83')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
