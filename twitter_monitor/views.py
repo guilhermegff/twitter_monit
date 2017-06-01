b0837b55d727d27fe4d517c03c1ff76b012a0874
guilhermegff <triclorotolueno@hotmail.com>
2017-05-31 23:56:58 -0700

    21 Heroku Commit

--- twitter_monitor/views.py
+++ twitter_monitor/views.py
@@ -5,13 +5,13 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect, QueryDict, HttpResponse, Http404
 from django.views import generic
 from django.core.paginator import Paginator, PageNotAnInteger
-from . import auth_mixins
+from auth_mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse, reverse_lazy
 import datetime
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from chartit import DataPool, Chart
-from . import got
+from got import *
 from .graficos import combview_total, combview_mensal, combview_semanal, barview
 
 def index(request):
